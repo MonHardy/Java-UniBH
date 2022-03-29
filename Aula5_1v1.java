af8086c8d04ffada342c083ce10b452ceeef46f2
Mon <munchenhardy@gmail.com>
2022-03-29 11:01:24 -0300

    Create Aula5_1v1.java

---
+++ Aula5_1v1.java
@@ -0,0 +1,49 @@
+package mypackage.aula5f_1v1;
+import java.util.Scanner;
+import java.io.*;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author MonHardy
+ * GitHub & Replit
+ */
+
+public class Aula5F_1v1 {
+
+    /**
+     * Exercício de Função Java -- Slide 5
+     * O que você quer, o que vai entrar e como.
+     */
+    
+    public static void main(String[] args) {
+        
+        // -------- Definição de variáveis --------
+        double salario, imposto; 
+        Scanner ler = new Scanner(System.in);
+        
+        // -------- Decimal -------
+        DecimalFormat deci = new DecimalFormat("0.00");
+        
+        // -------- Entrada de dados --------
+        System.out.println("Digite seu salário: ");
+        salario = ler.nextDouble();
+        
+        // -------- Definição de função --------
+        imposto=CalculaImposto(salario);
+        
+        // -------- Entrada de dados --------
+        System.out.println("Você pagará em impostos: R$" + deci.format(imposto));   
+    }
+    
+        // -------- Função --------
+        //Não precisa vir depois do código.
+    
+    public static double CalculaImposto(Double x){
+        double desconto;
+        desconto=x*0.14;
+        
+        // -------- Retorno de dado --------
+        return desconto;
+    }
+}
