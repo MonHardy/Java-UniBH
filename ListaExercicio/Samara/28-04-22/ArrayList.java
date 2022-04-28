bbbec16ddb5e6fddd0d653f19b291ae73595e105
Carla C <munchenhardy@gmail.com>
2022-04-28 10:19:25 -0300

    Create ArrayList.java

---
+++ ListaExercicio/Samara/28-04-22/ArrayList.java
@@ -0,0 +1,36 @@
+package samara.pkg28.pkg04;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+/* Faça um algoritmo que leia 5 números inteiro e armazene em um arraylist e 
+mostre uma mensagem indicando se cada número é par ou ímpar, 
+e se é positivo ou negativo.
+*/
+
+public class Samara2804 {
+
+    public static void main(String[] args) {
+        int quantidade, numeroUsuario, parImpar;
+        Scanner ler = new Scanner(System.in);
+
+        ArrayList<Integer> ListaNumeroUsuarios = new ArrayList<>();
+        
+        System.out.println("Digite a quantidade de números: ");
+        quantidade = ler.nextInt();
+        
+        for(int i=1; i<=quantidade; i++){
+        System.out.println("Digite os números desejados: ");
+        numeroUsuario = ler.nextInt(); 
+        
+        ListaNumeroUsuarios.add(numeroUsuario);
+        
+                parImpar = numeroUsuario % 2;
+        if (parImpar == 0){
+            System.out.println("Esse número é par");
+        }else{
+            System.out.println("Esse número é ímpar");
+            }
+        }      
+    }
+}
